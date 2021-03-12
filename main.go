5d814dca8b717ea77cca7d273b6b150228ceda27
mmuschner <mmuschner@localhost.localdomain>
2021-03-12 18:18:07 +0100

    Added zerolog to main

--- main.go
+++ main.go
@@ -2,32 +2,50 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"runtime/pprof"
+	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/uyuni-project/inter-server-sync/cli"
 	"github.com/uyuni-project/inter-server-sync/dumper"
 	"github.com/uyuni-project/inter-server-sync/schemareader"
 )
 
-func init() { log.SetFlags(log.Lshortfile | log.LstdFlags) }
+// func init() { log.SetFlags(log.Lshortfile | log.LstdFlags) }
+
+var Logfile string
 
 func main() {
+	const layout = "01-02-2006"
+	now := time.Now()
+	Logfile := "uyuni_iss_log_" + now.Format(layout) + ".json"
+	lf, err := os.OpenFile(Logfile, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if os.IsNotExist(err) {
+		f, err := os.Create(Logfile)
+		if err != nil {
+			log.Error().Msg("Unable to create logfile")
+		}
+		lf = f
+	}
+	logger := zerolog.New(lf).With().Timestamp().Logger().Output(lf)
 	parsedArgs, err := cli.CliArgs(os.Args)
 	if err != nil {
+		logger.Fatal().Err(err).Msg("")
 		os.Exit(1)
 	}
 
 	if parsedArgs.Cpuprofile != "" {
 		f, err := os.Create(parsedArgs.Cpuprofile)
 		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
+			logger.Fatal().Err(err).Msg("Could not create CPU profile")
 		}
+		logger.Info().Msg("CPU profile parsed")
 		defer f.Close() // error handling omitted for example
 		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
+			logger.Fatal().Err(err).Msg("Could not start CPU profile")
 		}
 		defer pprof.StopCPUProfile()
 	}
@@ -65,12 +83,12 @@ func main() {
 	if parsedArgs.Memprofile != "" {
 		f, err := os.Create(parsedArgs.Memprofile)
 		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
+			logger.Fatal().Err(err).Msg("Could not create memory profile")
 		}
 		defer f.Close() // error handling omitted for example
 		//runtime.GC()    // get up-to-date statistics
 		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
+			logger.Fatal().Err(err).Msg("Could not write memory profile")
 		}
 	}
 
